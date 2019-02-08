72f2ac57ea0ddb09f6e16be2c1f265c5a273374f
Savannah Cook <savannahcook@Savannahs-MacBook-Pro.local>
2019-02-08 14:58:57 -0700

    got auth running

---
+++ src/component/Post/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Post extends Component{
+    render(){
+        return(
+            <div>
+                This is the Post Component
+            </div>
+        )
+    }
+}
+
+export default Post;
